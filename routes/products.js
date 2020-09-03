b737a99716b416d75f5580834a216f6ccec73ccf
Carlos Revilla <clro26.k@gmail.com>
2020-09-03 23:40:04 +0200

    added table bbdd

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const { database } = require('../config/helpers');
+const {
+  database
+} = require('../config/helpers');
 
 
 
 /* GET ALL PRODUCTS */
-router.get('/', function(req, res) {
-  let page = (req.query.page !== undefined && req.query.page !== 0 ) ? req.query.page : 1;
-  const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10; 
+router.get('/', function (req, res) {
+  let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
+  const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;
 
+  let startValue;
+  let endValue;
+
+  if (page > 0) {
+    startValue = (page * limit) - limit;
+    endValue = page * limit;
+  } else {
+    startValue = 0;
+    endValue = 10;
+  }
+
+  database.table('products as p')
+    .join([{
+      table: 'categories as c',
+      on: 'c.id = p.cat_id'
+    }])
+    .withFields(['c.title as category',
+      'p.title as name',
+      'p.price',
+      'p.quantity',
+      'p.image',
+      'p.id'
+    ])
 });
 
 module.exports = router;
