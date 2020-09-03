489db7f1e781af995e2a5bc37e5b76053c1045ce
Carlos Revilla <clro26.k@gmail.com>
2020-09-03 23:51:51 +0200

    proomise and refactoring

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const app = express();
-
-
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+const cors = require('cors');
+
+
 
 // Import Routes
 const productsRouter = require('./routes/products');
@@ -29,7 +29,7 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', productsRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
