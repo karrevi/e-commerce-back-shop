286ce288694a6dac2c46aa92c346c2aa541c20a1
Carlos Revilla <clro26.k@gmail.com>
2020-09-04 23:49:27 +0200

    filter order by id

--- routes/orders.js
+++ routes/orders.js
@@ -36,4 +36,40 @@ router.get('/', (req, res) => {
     }).catch(err => console.log(err));
 });
 
+// GET SINGLE ORDER
+router.get('/:id', async (req, res) => {
+  let orderId = req.params.id;
+  console.log(orderId);
+
+  database.table('orders_details as od')
+    .join([{
+        table: 'orders as o',
+        on: 'o.id = od.order_id'
+      },
+      {
+        table: 'products as p',
+        on: 'p.id =od.product.id'
+      },
+      {
+        table: 'users as u',
+        on: 'u.id = o.user_id'
+      }
+    ])
+    .withFields(['o.id', 'p.title', 'p.description', 'p.price', 'p.image', 'od.quantity as  quantityOrdered'])
+    .filter({
+      'o.id': orderId
+    })
+  getAll()
+    .then(orders => {
+      if (orders.length > 0) {
+        res.json(orders);
+      } else {
+        res.json({
+          message: 'No orders found'
+        });
+      }
+    }).catch(err => res.json(err));
+
+});
+
 module.exports = router;
